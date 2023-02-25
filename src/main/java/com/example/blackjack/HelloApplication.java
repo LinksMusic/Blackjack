d9469b70a88ca3dc33f2f60289ba9acad2dd31c8
René Fabian Ripplinger <reneripp@mail.uni-paderborn.de>
2023-02-25 18:54:22 +0100

    Add Dummy Code

--- src/main/java/com/example/blackjack/HelloApplication.java
+++ src/main/java/com/example/blackjack/HelloApplication.java
@@ -1,11 +1,16 @@
 package com.example.blackjack;
 
+import com.example.blackjack.datatypes.Deck;
+import com.example.blackjack.datatypes.Player;
+import com.example.blackjack.datatypes.enums.Chip;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HelloApplication extends Application {
     @Override
@@ -18,6 +23,11 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        //launch();
+        ArrayList<Chip> list = new ArrayList<>();
+        list.add(Chip.TEN);
+        Player p = new Player("Dummy",list);
+        p.hit();
+        p.hit();
     }
 }
